6cbf27ab9222d3bed187a2d3320ebc4ed2a4455d
Syed Faizaan Mustafa <100984813+FaizaanMustafa@users.noreply.github.com>
2026-02-14 18:50:12 -0800

    Fix syntax for incremental loading condition

--- dbt_airbnb_prj/snowflake_airbnb_project/models/bronze/bronze_bookings.sql
+++ dbt_airbnb_prj/snowflake_airbnb_project/models/bronze/bronze_bookings.sql
@@ -5,5 +5,5 @@
 select * from {{ source('sources', 'bookings') }}
 
 {% if incremental_flag ==1 %}
-    where {{incremental_column}} >= (select coalesce(max('{{incremental_column}}'), '1900-01-01') from {{ this }})
+    where {{incremental_column}} >= (select coalesce(max({{incremental_column}}), '1900-01-01') from {{ this }})
 {% endif %}
