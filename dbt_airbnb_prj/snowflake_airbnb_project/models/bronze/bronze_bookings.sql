efe65048137d5c5465722b858ca2fed3afab8df1
Syed Faizaan Mustafa <100984813+FaizaanMustafa@users.noreply.github.com>
2026-02-14 18:09:10 -0800

    Update bronze_bookings.sql

--- dbt_airbnb_prj/snowflake_airbnb_project/models/bronze/bronze_bookings.sql
+++ dbt_airbnb_prj/snowflake_airbnb_project/models/bronze/bronze_bookings.sql
@@ -1,7 +1,9 @@
-{{config(materialized='incremental')}}
+{% set incremental_flag = 1%}
+{% set incremental_colum = 'created_at' %}
+
 
 select * from {{ source('sources', 'bookings') }}
 
-{% if is_incremental() %}
-    where created_at >= (select coalesce(max(created_at), '1900-01-01') from {{ this }})
+{% if incremental_flag ==1 %}
+    where {{incremental_column}} >= (select coalesce(max('{{incremental_column}}'), '1900-01-01') from {{ this }})
 {% endif %}
